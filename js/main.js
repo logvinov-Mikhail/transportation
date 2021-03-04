f339af4e7830208d4a0fe3c1f30e1fdcec945ea4
Logvinov Mikhail <mlogvinov2016@gmail.com>
2021-03-04 20:17:52 +0300

    slider__css

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(function(){
-
     $('.truck__slider').slick({
         nextArrow:'<button class="slick-arrow slick-next"> <img src="images/next.svg" alt="next arrow"></button>',
         prevArrow:'<button class="slick-arrow slick-prev"> <img src="images/prev.svg" alt="prev arrow"></button>',
@@ -9,44 +8,7 @@ $(function(){
         autoplay: false,
         fade: true,
         responsive: [
-            // {
-            //     breakpoint: 441,
-            //     settings: {
-            //         arrows: false
-            //     }
-            // }
+
         ]
     });
-
-    // $('.reviews__slider').slick({
-    //     arrows: false,
-    //     dots: true,
-    //     slidesToShow: 4,
-    //     slidesToScroll: 1,
-    //     responsive: [
-    //         {
-    //             breakpoint: 1141,
-    //             settings: {
-    //                 slidesToShow: 3,
-    //           }
-    //         },
-    //         {
-    //             breakpoint: 846,
-    //             settings: {
-    //                 slidesToShow: 2,
-    //           }
-    //         },
-    //         {
-    //             breakpoint: 585,
-    //             settings: {
-    //                 slidesToShow: 1,
-    //           }
-    //         },
-    //     ]
-    // });
-
-    // $('.menu__btn').on('click', function(){
-    //     $('.menu__list').toggleClass('menu__list--active');
-    // })
-
 });
